8df8a4eaf0810c89e88b645e8127ef0c1f65726d
Yusuf Britton <yusufbritton@yahoo.com>
2020-07-28 08:10:26 -0400

    anime recommendation connected to Postgres

--- anime_recommender.py
+++ anime_recommender.py
@@ -1,4 +1,4 @@
-from postgres_credentials import anime_query, ratings_query, merge_subset_query, merge_query
+import postgres_credentials as pg
 
 import time
 
@@ -16,83 +16,67 @@ import seaborn as sns
 sns.set_style('whitegrid')
 # %matplotlib inline #Used in jupyter notebook
 
-
 start = time.time()
 
-# Assigning Dataframes
-# TODO: Replace with database connection
-# rating_df = pd.read_csv('../data/rating.csv')
-# anime_df = pd.read_csv('../data/anime.csv')
 
-rating_df = pd.DataFrame(ratings_query(), columns=['user_id','anime_id','rating
+"""Assigning Dataframes"""
 
-anime_df = pd.DataFrame(anime_query(), columns=['anime_id','name','genre', 'type', 'episodes', 'rating', 'members'])
+print('Querying Animes')
+# rating_df = pd.read_sql_query(pg.ratings_query(),pg.pg_connection())
+anime_df = pd.read_sql_query(pg.anime_query(),pg.pg_connection())
+# merged_df = pd.read_sql_query(pg.merge_query(),pg.pg_connection())
+print('Completed in: ', '{:0.2f}'.format(time.time()-start))
 
-print("Printing ratings_df")
-print(rating_df.head())
+chosen_anime = 'Mushishi'
+param = {'anime' : chosen_anime}
 
-# # Merge Dataframes and subset
-# # TODO: Running into a memory problem. For now, saving DF
-# merged_df = pd.merge(rating_df,anime_df.drop('rating',axis=1),on='anime_id')
-# # merged_df.to_csv('../data/merged_df.csv')
-#
-# #Temporary solution for memory problem
-# # merged_df = pd.read_csv('../data/merged_df.csv')
-#
-# merged_10k_df = merged_df[merged_df["members"] > 10000]
-# merged_10k_df = merged_10k_df[merged_10k_df["rating"] > -1]
-#
-# user_watched_df = merged_10k_df[['user_id', 'name']]# dataframe of users that watched animes
-# user_watched_df = user_watched_df.loc[user_watched_df['name'] == anime]
-#
-# # List of users who watched the chosen anime
-# unique_users_array = user_watched_df.user_id.unique()
-#
-# # Subset data to only include users that watched the chosen anime, including all other animes watched
-# user_subset_df = merged_10k_df[merged_10k_df.user_id.isin(unique_users_array)]
-#
-# # Subset anime_df based on unique users
-# # anime_df = anime_df[anime_df.user_id.isin(unique_users_array)]
-#
-# # Construct average scores of animes
-# rating_mean_df = pd.DataFrame(user_subset_df.groupby('name')['rating'].mean())
-# rating_mean_df['num of ratings'] = pd.DataFrame(user_subset_df.groupby('name')['rating'].count())
-#
-# # Pic the genres from this selection
-# # TODO: See if you have to subset anime_df also
-# genre_dict = pd.DataFrame(data=anime_df[['name','genre']])
-# genre_dict.set_index('name',inplace=True) #This is what is ingested
-#
-#
-# def check_genre(genre_list,string):
-#     if any(x in string for x in genre_list):
-#         return True
-#     else:
-#         return False
-#
-# def get_recommendation(name):
-#     #generating list of anime with the same genre with target
-#     anime_genre = genre_dict.loc[name].values[0].split(', ') #
-#     cols = anime_df[anime_df['genre'].apply(
-#         lambda x: check_genre(anime_genre,str(x)))]['name'].tolist()
-#
-#     #create matrix based on generated list
-#     animemat = user_subset_df[user_subset_df['name'].isin(cols)].pivot_table(
-#         index='user_id',columns='name',values='rating')
-#
-#     #create correlation table
-#     anime_user_rating = animemat[name]
-#     similiar_anime = animemat.corrwith(anime_user_rating)
-#     corr_anime = pd.DataFrame(similiar_anime,columns=['correlation'])
-#     corr_anime = corr_anime.join(rating_mean_df['num of ratings'])
-#     corr_anime.dropna(inplace=True)
-#     corr_anime = corr_anime[corr_anime['num of ratings'] > 1000].sort_values(
-#         'correlation',ascending=False)
-#
-#     return corr_anime.head(10)
-#
-# # Test
-# get_recommendation('Mushishi')
-#
-# end = time.time()
-# print(end - start)
+print('Querying subset for: ', chosen_anime)
+user_subset_df = pd.read_sql_query(pg.merge_subset_query(),
+                    pg.pg_connection(), params=param)
+print('Completed in: ', '{:0.2f}'.format(time.time()-start))
+
+"""Creating relationship tables"""
+# Construct average scores of animes
+rating_mean_df = pd.DataFrame(user_subset_df.groupby('name')['rating'].mean())
+rating_mean_df['num of ratings'] = pd.DataFrame(user_subset_df.groupby('name')['rating'].count())
+
+# Pic the genres from this selection
+genre_dict = pd.DataFrame(data=anime_df[['name','genre']])
+genre_dict.set_index('name',inplace=True) #This is what is ingested
+
+"""Recommendation functions"""
+def check_genre(genre_list,string):
+    if any(x in string for x in genre_list):
+        return True
+    else:
+        return False
+
+# TODO: When looking a anime with a small poll of viewing, dynamically update ['num of ratings']
+def get_recommendation(name):
+    #generating list of anime with the same genre with target
+    anime_genre = genre_dict.loc[name].values[0].split(', ') #
+    cols = anime_df[anime_df['genre'].apply(
+        lambda x: check_genre(anime_genre,str(x)))]['name'].tolist()
+
+    #create matrix based on generated list
+    animemat = user_subset_df[user_subset_df['name'].isin(cols)].pivot_table(
+        index='user_id',columns='name',values='rating')
+
+    #create correlation table
+    anime_user_rating = animemat[name]
+    similiar_anime = animemat.corrwith(anime_user_rating)
+    corr_anime = pd.DataFrame(similiar_anime,columns=['correlation'])
+    corr_anime = corr_anime.join(rating_mean_df['num of ratings'])
+    corr_anime.dropna(inplace=True)
+    corr_anime = corr_anime[corr_anime['num of ratings'] > 1000].sort_values(
+        'correlation',ascending=False)
+
+    return corr_anime.head(10)
+
+# Test
+print('Gathering Recommendations: ', chosen_anime)
+print(get_recommendation(chosen_anime))
+
+
+end = time.time()
+print("Full run of code: ", '{:0.2f}'.format(time.time()-start))
--- postgres_credentials.py
+++ postgres_credentials.py
@@ -8,178 +8,101 @@ import pandas as pd
 import time
 
 # https://pynative.com/python-postgresql-select-data-from-table/
+def pg_connection():
+    connection = psycopg2.connect(user = os.environ['PG_USER'],
+                              password = os.environ['POSTGRES_PASS'],
+                              host = "127.0.0.1",
+                              port = "5432",
+                              database = os.environ['PG_DB'])
+    return connection
 
 def anime_query():
-    t = time.time()
-    try:
-        # Connecting to PostgreSQL
-        connection = psycopg2.connect(user = os.environ['PG_USER'],
-                                      password = os.environ['POSTGRES_PASS'],
-                                      host = "127.0.0.1",
-                                      port = "5432",
-                                      database = os.environ['PG_DB'])
+    pg_query = """
+    SELECT *
+    FROM mal_anime
+    """
 
-        #
-        cursor = connection.cursor()
-
-        pg_query = """
-        SELECT *
-        FROM mal_anime
-        """
-
-        cursor.execute(pg_query)
-        print("Selecting rows from anime table using cursor.fetchall")
-
-        anime_data = np.array(cursor.fetchall())
-
-        print(anime_data)
-        print("Number of rows: ", len(anime_data,))
-        print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
-        return(anime_data)
-
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while fetching data from PostgreSQL: ", error)
-
-    finally:
-        #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
-
-    print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
+    return pg_query
 
 def ratings_query():
-    t = time.time()
-    try:
-        # Connecting to PostgreSQL
-        connection = psycopg2.connect(user = os.environ['PG_USER'],
-                                      password = os.environ['POSTGRES_PASS'],
-                                      host = "127.0.0.1",
-                                      port = "5432",
-                                      database = os.environ['PG_DB'])
-
-        #
-        cursor = connection.cursor()
-
-        pg_query = """
-        SELECT *
-        FROM mal_ratings
-        """
-
-        cursor.execute(pg_query)
-        print("Selecting rows from ratings table using cursor.fetchall")
-
-        anime_data = np.array(cursor.fetchall())
-
-        print(anime_data)
-        print("Number of rows: ", len(anime_data,))
-        print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
-        return(anime_data)
-
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while fetching data from PostgreSQL: ", error)
-
-    finally:
-        #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
-
-    print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
+    pg_query = """
+    SELECT *
+    FROM mal_ratings
+    """
+    return pg_query
 
 def merge_query():
-    t = time.time()
-    try:
-        # Connecting to PostgreSQL
-        connection = psycopg2.connect(user = os.environ['PG_USER'],
-                                      password = os.environ['POSTGRES_PASS'],
-                                      host = "127.0.0.1",
-                                      port = "5432",
-                                      database = os.environ['PG_DB'])
+    pg_query = """
+    SELECT *
+    FROM merged_ratings
+    """
 
-        #
-        cursor = connection.cursor()
+    return pg_query
 
-        pg_query = """
-        SELECT *
-        FROM merged_ratings
-        """
+def merge_subset_query():
+    pg_query = """
+    WITH chosen_anime_users AS (
+	SELECT DISTINCT user_id
+	FROM merged_ratings
+	WHERE name = %(anime)s --Will use python to insert this
+    )
 
-        cursor.execute(pg_query)
-        print("Selecting rows from anime table using cursor.fetchall")
+    SELECT mr.*
+    FROM merged_ratings mr
+    JOIN chosen_anime_users ca
+    ON mr.user_id = ca.user_id
+    WHERE mr.user_id = ca.user_id;
+    """
 
-        anime_data = np.array(cursor.fetchall())
+    return pg_query
 
-        print(anime_data)
-        print("Number of rows: ", len(anime_data,))
-        print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
-        return(anime_data)
 
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while fetching data from PostgreSQL: ", error)
-
-    finally:
-        #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
-
-    print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
-
-def merge_subset_query(anime_chosen):
-    t = time.time()
-    try:
-        # Connecting to PostgreSQL
-        connection = psycopg2.connect(user = os.environ['PG_USER'],
-                                      password = os.environ['POSTGRES_PASS'],
-                                      host = "127.0.0.1",
-                                      port = "5432",
-                                      database = os.environ['PG_DB'])
-
-        #
-        cursor = connection.cursor()
-
-        pg_query = """
-        WITH chosen_anime_users AS (
-    	SELECT DISTINCT user_id
-    	FROM merged_ratings
-    	WHERE name = %s --Will use python to insert this
-        )
-
-        SELECT *
-        FROM merged_ratings mr
-        JOIN chosen_anime_users ca
-        ON mr.user_id = ca.user_id
-        WHERE mr.user_id = ca.user_id;
-        """
-
-        cursor.execute(pg_query, (str(anime_chosen),))
-        print("Selecting rows from anime table using cursor.fetchall")
-
-        anime_data = np.array(cursor.fetchall())
-
-        print(anime_data)
-        print("Number of rows: ", len(anime_data,))
-
-        print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
-        return(anime_data)
-
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while fetching data from PostgreSQL: ", error)
-
-    finally:
-        #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
-
-    print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
-
-# merge_subset_query('Mushishi')
-# anime_query()
-# ratings_query()
-# merge_query()
+# def local_pg_query(anime_chosen):
+#     t = time.time()
+#     try:
+#         # Connecting to PostgreSQL
+#         connection = psycopg2.connect(user = os.environ['PG_USER'],
+#                                       password = os.environ['POSTGRES_PASS'],
+#                                       host = "127.0.0.1",
+#                                       port = "5432",
+#                                       database = os.environ['PG_DB'])
+#
+#         #
+#         cursor = connection.cursor()
+#
+#         pg_query = """
+#         WITH chosen_anime_users AS (
+#     	SELECT DISTINCT user_id
+#     	FROM merged_ratings
+#     	WHERE name = %s --Will use python to insert this
+#         )
+#
+#         SELECT mr.*
+#         FROM merged_ratings mr
+#         JOIN chosen_anime_users ca
+#         ON mr.user_id = ca.user_id
+#         WHERE mr.user_id = ca.user_id;
+#         """
+#
+#         cursor.execute(pg_query, (str(anime_chosen),))
+#         print("Selecting rows from subset table using cursor.fetchall")
+#
+#         anime_data = np.array(cursor.fetchall())
+#
+#         print(anime_data)
+#         print("Number of rows: ", len(anime_data,))
+#
+#         print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
+#         return(anime_data)
+#
+#     except (Exception, psycopg2.Error) as error :
+#         print ("Error while fetching data from PostgreSQL: ", error)
+#
+#     finally:
+#         #closing database connection.
+#             if(connection):
+#                 cursor.close()
+#                 connection.close()
+#                 print("PostgreSQL connection is closed")
+#
+#     print("Query completed in seconds: ", '{:0.2f}'.format(time.time()-t))
