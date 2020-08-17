11724d12cb6b5178497c1d02e316ca10633e216f
Yusuf Britton <yusufbritton@yahoo.com>
2020-08-16 20:14:13 -0400

    added session retry for 503 error

--- mal_jikan_users.py
+++ mal_jikan_users.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 import pandas as pd
 import  numpy as np
@@ -12,6 +14,7 @@ reference: https://jikan.docs.apiary.io/#reference/0/anime
 
 data = []
 
+# TODO: Loop through initial list of users and get their friends
 # col_names = ['user_id', 'anime_id','rating']
 
 t = time.time()
@@ -22,45 +25,69 @@ Data extracted from REST API
 friends_list = []
 
 def get_friends(mal_user):
+    # time.sleep(2)
     global friends_list #To be able to append to global friends list
-    frieds_url = f"https://api.jikan.moe/v3/user/{mal_user}/friends"
-    friends_r = requests.get(frieds_url)
 
-    if friends_r.ok:
-        friends_json = friends_r.json()
-        # print(json.dumps(friends_json, indent=2)) #pretty print
+    """This isn't needn't since we are not looping through users."""
+    # frieds_url = f"https://api.jikan.moe/v3/user/{mal_user}/friends"
+    # friends_r = requests.get(frieds_url)
+    #
+    # if friends_r.ok:
+    #     friends_json = friends_r.json()
+    #     print(json.dumps(friends_json, indent=2)) #pretty print
+    #
+    #     print(f'Username: ', mal_user)
 
+    page_loop = True
+    page_num = 1
+    friend_counter = 0
+    friends_list = []
+
+    while page_loop:
+        time.sleep(2)
         print(f'Username: ', mal_user)
+        print("\n Results from page ", page_num, "\n")
+        friends_url = f"https://api.jikan.moe/v3/user/{mal_user}/friends/{page_num}"
 
-        page_loop = True
-        page_num = 1
-        friend_counter = 0
-        friends_list = []
+        """In Case of 503 error, retry request"""
+        s = requests.Session()
 
-        while page_loop:
+        retries = Retry(total=10,
+                backoff_factor=0.5,
+                status_forcelist=[503])
 
-            print("\n Results from page ", page_num, "\n")
-            frieds_url = f"https://api.jikan.moe/v3/user/{mal_user}/friends/{page_num}"
-            friends_r = requests.get(frieds_url)
-            friends_json = friends_r.json()
+        s.mount('https://', HTTPAdapter(max_retries=retries))
 
-            for friend in friends_json['friends']:
-                friends_list.append(friend['username'])
-                print(f'User: ', friend['username'])
 
-                friend_counter += 1
-                print(f"friends counted: ",friend_counter)
+        # friends_r = requests.get(friends_url) #getting 503 error
+        friends_r = s.get(friends_url)
+        friends_json = friends_r.json()
 
-            try:
-                friends_json['friends'][99]
-            except IndexError:
-                page_loop = False
-                return
+        for friend in friends_json['friends']:
+            friends_list.append(friend['username'])
+            print(f'User: ', friend['username'])
 
-            page_num += 1
-            time.sleep(2)
+            friend_counter += 1
+            print(f"friends counted: ",friend_counter)
 
-        data.append(friends_list)
+        """Exceptions"""
+
+        # Checking to see if there are more friends on the page
+        try:
+            friends_json['friends'][99]
+        except IndexError:
+            page_loop = False
+            return
+
+        # If increment of 100, come out of the function
+        try:
+            friends_json['friends'][0]
+        except IndexError:
+            page_loop = False
+            return
+
+        """Go to the next page"""
+        page_num += 1
 
     else:
         print(f'{mal_user} has no friends :(')
@@ -70,11 +97,17 @@ def get_friends(mal_user):
 # get_friends('Nekomata1037') #Has 36 friends
 # get_friends('ysyouth') # has no friends :(
 # get_friends('spacecowboy') #over 4,000 friends
+# get_friends('BobSamurai') #over 1,500
+# get_friends('etekusat') #over 6,000
 
+
+"""Appending user list"""
 new_df = pd.DataFrame(friends_list, columns=['users'])
 
 df = pd.read_csv('../data/friends.csv')
 df = df.append(new_df, ignore_index=True)
+
+df.drop_duplicates(inplace=True)
 
 df.to_csv('../data/friends.csv', index=False)
 friends_list.clear()
