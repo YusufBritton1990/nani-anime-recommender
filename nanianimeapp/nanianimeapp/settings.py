3b5ae0597ca7d8ea725b1288a4a10c7e5b0c22c5
Yusuf Britton <yusufbritton@yahoo.com>
2020-10-10 21:30:31 -0400

    migrate postgres models

--- nanianimeapp/nanianimeapp/settings.py
+++ nanianimeapp/nanianimeapp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'nanianimeapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'NANI_ANIME',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['POSTGRES_PASS'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- nanianimeapp/recommender/models.py
+++ nanianimeapp/recommender/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class mal_anime_prod(models.Model):
+    anime_id = models.IntegerField(primary_key=True)
+    title_japanese = models.CharField(max_length=200)
+    title_english = models.CharField(null=True, max_length=200)
+    image_url = models.URLField(null=True)
+    anime_type = models.CharField(null=True, max_length=200)
+    genres = models.CharField(null=True, max_length=200)
+    episodes = models.IntegerField(null=True)
+    mal_url = models.URLField(null=True)
+    synopsis = models.CharField(null=True, max_length=50000)
+    trailer_url = models.URLField(null=True)
+    rating = models.DecimalField(null=True,max_digits=4,decimal_places=2)
+    members = models.IntegerField(null=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+
+class mal_user_prod(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+class mal_rating_prod(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(mal_user_prod, on_delete=models.CASCADE)
+    anime_id = models.ForeignKey(mal_anime_prod, on_delete=models.CASCADE)
+    rating = models.IntegerField(null=True)
+    update_time = models.DateTimeField(auto_now=True)
